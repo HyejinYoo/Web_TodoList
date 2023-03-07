5d5623f5c22f2d12a5cddda2bc2dfae87aa0e268
HyejinYoo <104633224+HyejinYoo@users.noreply.github.com>
2023-03-07 15:41:35 +0900

    _

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//var bodyParser = require('body-parser');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,7 +12,4 @@ root.render(
 );
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
